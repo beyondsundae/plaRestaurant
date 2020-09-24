86115e9348fd1e698c7aebd9fa64b5735b53684d
beyondsundae <p00000rice@gmail.com>
2020-09-25 00:36:56 +0700

    Fixed fuction button, Can Add, Delete, ClearAll, Increase and Decrease Amount, Switch each nation Food

--- src/Components/MenuPage.js
+++ src/Components/MenuPage.js
@@ -1,147 +1,79 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const MenuPage =()=> {
-    let amount = 0
 
-    const [Menu, setMenu] = useState([
-        {
-            food:"ข้าวเปล่า",
-            price:10,
-            status:"NoAdded"
-        },
-        {
-            food:"ข้าวกะเพราหมูสับ",
-            price:45,
-            status:"NoAdded"
-        }
-    ])
-
-    const [Order, setOrder] = useState([
-        // {
-        //     food:"ข้าวเปล่า",
-        //     amount:1,
-        //     price:10,
-        //     status:"Added"
-        // },
-        // {
-        //     food:"ข้าวกะเพราหมูสับ",
-        //     amount:2,
-        //     price:45,
-        //     status:"NoAdded"
-        // }
-    ])
-    const [Orderx, setOrderx] = useState(
-        {
-            food:"ข้าวเปล่า",
-            amount:1,
-            price:10,
-            status:"Added"
-        },
-        {
-            food:"ข้าวกะเพราหมูสับ",
-            amount:2,
-            price:45,
-            status:"Added"
-        }
-    )
-
-    const [selected, setSelected] = useState([])
-
+    const [Menu, setMenu] = useState([])
+    const [Order, setOrder] = useState([])
     const [OrderTotal, setOrdertotal] = useState(0)
 
-    const BuddhFood =()=>{
-        setMenu([
-            {
-                food:"ข้าวเปล่า",
-                price:10
-            },
-            {
-                food:"ข้าวกะเพราหมูสับ",
-                price:45
-            }
-        ])
+    const [BuddhFood, setBuddhFood] = useState([
+        {
+            food:"ข้าวเปล่า",
+            price:10,
+            status:"NoAdded",
+            nation:"Buddh"
+        },
+        {
+            food:"ข้าวกะเพราหมูสับ",
+            price:45,
+            status:"NoAdded",
+            nation:"Buddh"
+        }
+    ])
+    const [IslamFood, setIslamFood] = useState([
+    {
+        food:"ข้าวกะเพราไก่",
+        price:45,
+        status:"NoAdded",
+        nation:"Islam"
+    }])
 
+    const SetInitialBuddhFood =()=>{
+       // let ChangeStatus =[...Menu]
+       // ChangeStatus[index] = {...ChangeStatus[index], status:"Added"}
+        let BuddhMenu = []  
+        for(let i = 0; i<BuddhFood.length; i++){
+            BuddhMenu.push(BuddhFood[i])
+        }
+        setMenu(BuddhMenu)
     }
 
-    const IslamFood =()=>{
-        setMenu([
-            {
-                food:"ข้าวเปล่า",
-                price:10
-            },
-            {
-                food:"ข้าวกะเพราไก่",
-                price:45
-            }
-        ])
-
+    const ChangeBuddhFood =()=>{
+        let BuddhMenu = []  
+        for(let i = 0; i<BuddhFood.length; i++){
+            BuddhMenu.push(BuddhFood[i])
+        }
+        setMenu(BuddhMenu)
     }
-
-    const AddOrderFirst =(item, index)=>{
-        // console.log(item.food)
-        let tempArr = [...Order]
-        // console.log(tempArr)
-        let temp =[...Menu]
-        temp[index] = {...temp[index], status:"Added"}
-        // for(let i=0; i<Menu.length; i++){
-        //     console.log(Menu[i].food)
-        // }
-        console.log(index)
-        Menu.splice(index, 1)
-
-
-        console.log("item ที่เข้ามา", item)
-        setOrder([...Order, {...item, amount:1} ])
-        // setMenu([...Menu, {...item, status:"Added"} ])}
-        setMenu(temp)}
-
-    const AddOrderSecond =(selectedItem, index)=>{
-         //console.log("ที่เลือกมา :", selectedItem, index)
-         let hardCopy = [...Order]
-         let origin = [ ...hardCopy]
-
-         let AddNewRow = [...hardCopy, {...selectedItem, amount:1}]
- 
-         hardCopy = hardCopy.filter((cartItem)=>{
-             console.log("ที่มีอยู่ใน Order :",cartItem)
-             
-             if(cartItem.food == selectedItem.food){
-                 // item ที่เลือก == ไอเทมที่มีใน Order
-                 console.log("Do if")
-                 return(
-                     cartItem.amount += 1
-                     )
-                 //ต้องคืนอาหารเดิม เเละ เพิ่มจำนวนอาหารที่เลือกไป
-             }
-             if(cartItem.food != selectedItem.food){
-                 console.log("Do else")
-                 
-                 return [...hardCopy, {...selectedItem, amount:1}]
-                    
-                 
-               // setOrder([...Order, {...selectedItem, amount:1} ])
-             }
-         })
-
-             console.log('hardCopy', ...hardCopy)
-            //  if(hardCopy[0] == true){
-            //      console.log("Do if")
-            //      if(hardCopy[0] == true){
-            //         hardCopy.shift()
-            //      }
-            //      else{
-            //         console.log([...hardCopy, {...selectedItem, amount:1}])
-            //     setOrder([...hardCopy, {...selectedItem, amount:1}])
-            //      }
-                 
-            //  }
-            //  else{
-            //     console.log("Do else")
-            //    setOrder(origin)  
-            //  }
-             setOrder(origin)  
-             
+    const ChangeIslamFood =()=>{
+        let IslamMenu = []  
+        for(let i = 0; i<IslamFood.length; i++){
+            //console.log(IslamFood[i])
+            IslamMenu.push(IslamFood[i])
+        }
+        setMenu(IslamMenu)
+    }
         
+    const AddBuddhFirst =(item, index)=>{
+        let ChangeStatus =[...BuddhFood]
+        ChangeStatus[index] = {...ChangeStatus[index], status:"Added"}
+        setBuddhFood(ChangeStatus)
+        // ChangeBuddhFood()
+        console.log(BuddhFood)
+        console.log("item ที่เข้ามา", item, index)
+        setOrder([...Order, {...item, amount:1} ])
+        console.log("Menu ที่มี", Menu)
+    }
+    const AddIslamFirst =(item, index)=>{
+        let ChangeStatus =[...IslamFood]
+        ChangeStatus[index] = {...ChangeStatus[index], status:"Added"}
+        setIslamFood(ChangeStatus)
+        // ChangeIslamFood()
+        console.log(IslamFood)
+        console.log("item ที่เข้ามา", item, index)
+        setOrder([...Order, {...item, amount:1} ])
+        console.log("Menu ที่มี", Menu)
     }
 
     const addAmount =(selectedItem)=>{
@@ -149,34 +81,35 @@ const MenuPage =()=> {
         let hardCopy = [...Order]
         let origin = [ ...hardCopy]
 
-        hardCopy = hardCopy.filter((cartItem)=>{
-            console.log("ที่มีอยู่ใน Order :",cartItem)
+        // hardCopy = hardCopy.filter((cartItem)=>{
+        //     console.log("ที่มีอยู่ใน Order :",cartItem)
             
-            if(cartItem.food == selectedItem.food){
-                // item ที่เลือก == ไอเทมที่มีใน Order
-                return(
-                    cartItem.amount += 1
-                    )
-                //ต้องคืนอาหารเดิม เเละ เพิ่มจำนวนอาหารที่เลือกไป
+        //     if(cartItem.food == selectedItem.food){
+        //         // item ที่เลือก == ไอเทมที่มีใน Order
+        //         return(
+        //             cartItem.amount += 1
+        //             )
+        //         //ต้องคืนอาหารเดิม เเละ เพิ่มจำนวนอาหารที่เลือกไป
+        //     }
+        // })
+
+        for (let i = 0; i<hardCopy.length; i++){
+            if(hardCopy[i].food == selectedItem.food){
+                hardCopy[i].amount += 1
             }
-        })
+        }
             console.log('ที่จะคืนไป Order', hardCopy)
             setOrder(origin)
     }
 
-    const minusAmount =(selectedItem)=>{
+    const minusAmount =(selectedItem, index)=>{
         //console.log("ที่เลือกมา :", selectedItem, index)
         let hardCopy = [...Order]
-        let origin 
-
 
         hardCopy = hardCopy.filter((cartItem)=>{
             //console.log("ที่มีอยู่ใน Order :",cartItem)
            
             if(cartItem.food == selectedItem.food){
-                console.log('item ที่ select มาเพื่อลบ:', selectedItem, 
-                "เทียบกับ", cartItem, "คำตอบคือ", cartItem.food == selectedItem.food)
-                console.log("Do if")
                 // item ที่เลือก == ไอเทมที่มีใน Order
                 
                     console.log("Minus Done")
@@ -184,37 +117,43 @@ const MenuPage =()=> {
                         cartItem.amount -= 1
                     )  
                 //ต้องคืนอาหารเดิม เเละ เพิ่มจำนวนอาหารที่เลือกไป
-
             } 
 
             if(cartItem.food !== selectedItem.food) {
-                console.log('item ที่ select มาเพื่อลบ:', selectedItem, 
-                "เทียบกับ", cartItem, "คำตอบคือ", cartItem.food != selectedItem.food)
-                console.log("Do else")
-
                 return(
                     cartItem.food != selectedItem.food
+                    
                     // return ทุกอย่างที่ไม่ใช่ตัวที่เลือกมา
                 )
             }
         })
+        
+
+         {/*for (let i = 0; i<hardCopy.length; i++){
+             console.log(hardCopy[i].food)
+             console.log(selectedItem.food)
+            if(hardCopy[i].food == selectedItem.food){
+                  (hardCopy[i].amount -= 1)
+             }
+
+             else if((hardCopy[i].food < selectedItem.food)){
+                 console.log("wow")
+                return  hardCopy.push(hardCopy[i].food  !== selectedItem.food)
+         }}*/}
             //console.log('ที่จะคืนไป Order', origin)
-            //checkpoint?origin = [...hardCopy]:origin = hardCopy
             console.log('hardCopy', hardCopy)
+            goBackBuddhStatus(selectedItem, index)
+            goBackIslamStatus(selectedItem, index)
             setOrder(hardCopy)
     }
     
-    const removeOrder =(selectedItem)=>{
+    const removeOrder =(selectedItem, index)=>{
         // item ที่เข้ามาคือ item ของตัวที่กดเลือกในช่องOrder
         let hardCopy = [...Order]
         // Copy array to 'hardCopy' becoz we not goona use original array
-
         hardCopy = hardCopy.filter((cartItem)=>{
             // เอามา loop เพื่อดูว่าใน Order นั้นมี object อะไรบ้างและจะเอา object ที่ลูปมาทำ condition แล้วจะคืนค่าจาการ condition ไป
             // console.log('ใน Order :', cartItem)
-           console.log('item ที่ select มา Condition เพื่อลบ:', selectedItem, 
-           "เทียบกับ", cartItem, "คำตอบคือ", cartItem.food !== selectedItem.food)
-
             return(
             cartItem.food !== selectedItem.food
             )
@@ -222,15 +161,56 @@ const MenuPage =()=> {
             //เปรียบเทียบว่าให้ return ของที่อยู่ในตะกร้ากลับไปโดยทที่จะไม่มีตัว item ที่เข้ามา
         })
         console.log("กูจะคืน :", hardCopy)
+        goBackBuddhStatus(selectedItem, index)
+        goBackIslamStatus(selectedItem, index)
         setOrder(hardCopy)
+    }
+
+    const goBackBuddhStatus =(item, index)=>{
+        //console.log("NoAdded", index)
+
+        const ClearALLBuddh = BuddhFood.map((list, index)=>{
+            if(item.food === list.food){
+                return({...list, status:"NoAdded"})
+            }
+            else{
+                return(list)
+            }
+        })
+        setBuddhFood(ClearALLBuddh)
+    }
+    const goBackIslamStatus =(item, index)=>{
+        //console.log("NoAdded", index)
+
+        const ClearALLIslam = IslamFood.map((list, index)=>{
+            if(item.food === list.food){
+                return({...list, status:"NoAdded"})
+            }
+            else{
+                return(list)
+            }
+        })
+        setIslamFood(ClearALLIslam)
+    }
+    const goBackALLStatus =()=>{
+        //let ChangeBuddhStatus =[...BuddhFood]
+        //ChangeBuddhStatus = {...ChangeBuddhStatus, status:"NoAdded"}
+
+        const ClearALLBuddh = BuddhFood.map((list, index)=>{
+                return({...list, status:"NoAdded"})
+        })
+        setBuddhFood(ClearALLBuddh)
+        
+        const ClearALLIslam = IslamFood.map((list, index)=>{
+            return({...list, status:"NoAdded"})
+        })
+        setIslamFood(ClearALLIslam)
     }
 
     const total =()=>{
         let totalVal = 0
         // Order.map((item)=>{
-            
         //    return totalVal+=item.price
-           
         // })
         // console.log(totalVal)
         for (let i = 0; i<Order.length; i++){
@@ -249,58 +229,65 @@ const MenuPage =()=> {
         var txt;
         if (window.confirm("ต้องการยกเลิกการรายการทั้งหมดใช่หรือไม่")) {
             setOrder([])
+            goBackALLStatus()
         } else {
             txt = "You pressed Cancel!";
         }
+    }
+
+    const ViewButton =({item, index})=>{
+        if(item.status == "NoAdded"){
+                if(item.nation == "Buddh"){
+                    return(
+                        <button className='btn btn-success'  
+                            // style={{marginRight: "-100px", marginLeft: '100px'}}
+                            onClick={()=>{AddBuddhFirst(item, index)}}>
+                        +
+                        </button>
+                        )  
+                } else {
+                    return(
+                        <button className='btn btn-danger'  
+                            // style={{marginRight: "-100px", marginLeft: '100px'}}
+                            onClick={()=>AddIslamFirst(item, index)}>
+                        +
+                        </button>
+                    )
+                }
+        } else{
+            return(
+            <button className='btn btn-primary'  
+                // style={{marginRight: "-100px", marginLeft: '100px'}}
+                onClick={()=>addAmount(item, index)}>
+            +
+            </button>
+            )
+        }
         
     }
 
-    const hopper =()=>{
-        for(let i=0; i<Order.length; i++){
-            if(Order.[i].status == "NoAdded"){
-              console.log("NoAdded", Order.[i])  
-            }
-            else{
-                console.log("Added", Order.[i])  
-            }
-            
-        }
-    }
-
-    const hopper2 =()=>{
-        for(let i=0; i<Order.length; i++){
-            if(Order.[i].status == "NoAdded"){
-                console.log("Do if")
-                return(
-                    <>
-                    <p>xxxx</p>
-                    </>
-                )
-            }
-            else{
-                return(
-                    null
-                    )
-            }
-            
-        }
-    }
 
     useEffect(()=>{
-        console.log("Menu ที่มี", Menu)
-        // hopper()
-        // hopper2()
-        // console.log(Object.keys(Order))
-        // Order.splice()
+        //ไม่ต้องใส่ก็ได้
+        let BuddhMenu = []  
+        for(let i = 0; i<BuddhFood.length; i++){
+            BuddhMenu.push(BuddhFood[i])
+        }
+        setMenu(BuddhMenu)
 
-        
-    }, [Menu])
+    }, [])
+
+    useEffect(()=>{
+        ChangeIslamFood()
+    },[IslamFood])
+    useEffect(()=>{
+        //ให้ Render อาหารบุดดุตอนเริ่ม
+        ChangeBuddhFood()
+    },[BuddhFood])
 
     useEffect(()=>{
         console.log("Order Total", Order)
-    }, [Order])
-
-    useEffect(()=>{
+        console.log("Menu ที่มี", Menu)
         total()
     }, [Order])
 
@@ -313,8 +300,9 @@ const MenuPage =()=> {
                     </div>
 
                     <div className="col-2">
-                        <button className='btn btn-info mx-2' onClick={()=>BuddhFood()}>พุทธ</button>
-                        <button className='btn btn-info mx-2' onClick={()=>IslamFood()}>อิสลาม</button>
+                        <button className='btn btn-info mx-2' onClick={()=>ChangeBuddhFood()}>พุทธ</button>
+                        <button className='btn btn-info mx-2' onClick={()=>ChangeIslamFood()}>อิสลาม</button>
+                        {/*<button onClick={()=>console.log("Menu Total", Menu)}>refresh</button>*/}
                     </div>
                     <div className="col">
                         
@@ -330,50 +318,23 @@ const MenuPage =()=> {
                         <tr>
                         <th scope="col">อาหาร</th>
                         <th scope="col">ราคา</th>
-                        <th ></th>
+                        <th></th>
+                        <th></th>
                         </tr>
                     </thead>
                    
                     <tbody>
                     {Menu.map((item, index)=>{
-                        // console.log(item)
                         return(
                         <tr key={item.food}>
                         <th>{item.food}</th>
-                        <th>{item.price} 
-                        {item.status == "NoAdded"
-                            ?<button className='btn btn-success'  
-                                    style={{marginRight: "-100px", marginLeft: '100px'}}
-                                    onClick={()=>AddOrderFirst(item, index)}>
-                            +
-                            </button>
-
-                            :<button className='btn btn-primary'  
-                                    style={{marginRight: "-100px", marginLeft: '100px'}}
-                                    onClick={()=>AddOrderSecond(item, index)}>
-                            +
-                            </button>}
-                            
-                        
-                            {/* <button className='btn btn-success'  
-                                    style={{marginRight: "-100px", marginLeft: '100px'}}
-                                    onClick={()=>AddOrderFirst(item, index)}>
-                            +
-                            </button>
-                            <button className='btn btn-primary'  
-                                    style={{marginRight: "-100px", marginLeft: '100px'}}
-                                    onClick={()=>AddOrderSecond(item, index)}>
-                            +
-                            </button> */}
-                            
-                            
-                        </th>
+                        <th>{item.price} </th>
+                        <th> <ViewButton item={item} index={index}/></th>
                         <th>{item.status} </th>
                         </tr>
                     )})}
                     </tbody>
                     </table>
-                    {hopper2}
                 </div>
 
                 <div className="col-4 border border-danger"  style={{height: "auto", padding: "0px"}}>
@@ -389,6 +350,8 @@ const MenuPage =()=> {
                         <th scope="col">อาหาร</th>
                         <th scope="col">จำนวน</th>
                         <th scope="col">ราคา</th>
+                        <th></th>
+                        {/*<th></th>*/}
                         </tr>
                     </thead>
                    
@@ -401,7 +364,10 @@ const MenuPage =()=> {
                             <button className="mx-2 btn-dark" onClick={()=>minusAmount(item, index)}>-</button>
                                 {item.amount}
                             <button className="mx-2 btn-dark" onClick={()=>addAmount(item, index)}>+</button></th>
-                        <th>{item.price * item.amount} <button className="btn-dark ml-2" onClick={()=>removeOrder(item, index)}>ล้าง</button></th>
+                        <th>{item.price * item.amount} </th>
+                        <th><button className="btn-dark ml-2" onClick={()=>removeOrder(item, index)}>ล้าง</button></th>
+                        {/*<th><button className="btn-success ml-2" onClick={()=>goBackBuddhStatus(item, index)}>x</button>
+                        <button className="btn-danger ml-2" onClick={()=>goBackIslamStatus(item, index)}>x</button></th>*/}
                         </tr>
                     )})}
                     </tbody>
